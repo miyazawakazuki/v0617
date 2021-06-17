c268b0f07257b1d13e00333f3433366a002a56c1
A0601-07 <tnk.dat.class@gmail.com>
2021-06-17 09:52:57 +0900

    復習

--- v0617/Form1.cs
+++ v0617/Form1.cs
@@ -12,10 +12,47 @@ namespace v0617
 {
     public partial class Form1 : Form
     {
+
+        int vx =-10;
+        int vy =-10;
+
+        
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if(label1.Left<0)
+            {
+                vx = Math.Abs(vx);
+            }
+
+            if(label1.Top<0)
+            {
+                vy = Math.Abs(vy);
+            }
+
+            if(label1.Right>ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+
+            if(label1.Bottom>ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            vx = Math.Abs(vx) * 0;
+            vy = Math.Abs(vy) * 0;
+        }
     }
 }
